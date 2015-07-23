606e8ddd232c619822dc57f1da16b921d9160dbe
Kevin Wang <wangkevin448@gmail.com>
2015-07-23 14:05:01 -0700

    first commit

---
+++ voatenhancement.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Voat enhancement
+// @namespace    xor.kevin
+// @version      0.1
+// @description  my custom enhancement script for voat
+// @author       xorkevin
+// @match        *://*.voat.co/*
+// @grant        none
+// @required     https://code.jquery.com/jquery-2.1.4.min.js
+// ==/UserScript==
+
+//open all voat links (both submission and comments) in a new tab
+$(document).ready(function() {
+  $('div.submission > entry').children('p.title > a.title, p.tagline > a.author, p.tagline > a.subverse, ul.buttons > li.first > a.comments').attr('target', '_blank');
+});
